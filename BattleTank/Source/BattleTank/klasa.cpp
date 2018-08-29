654ae8c05df8cd6972754254f207c7fafc15dbb1
Rumir125 <34058167+Rumir125@users.noreply.github.com>
2018-08-29 09:11:27 +0200

    105 to 107 Landscape customization

---
+++ BattleTank/Source/BattleTank/klasa.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "klasa.h"
+
+
+// Sets default values
+Aklasa::Aklasa()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void Aklasa::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void Aklasa::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	UE_LOG(LogTemp, Warning, TEXT("Playing a game"));
+
+}
+
